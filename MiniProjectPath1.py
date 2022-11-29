c4a893c44e1e3c22bbeaca58212b3b3ed0975e3b
Ethan Cho <ethan@Ethans-MacBook-Air.local>
2022-11-29 05:17:03 -0500

    세이브를 잘 하자 ㅠ ㅋ큐ㅠㅠㅠㅠ

--- MiniProjectPath1.py
+++ MiniProjectPath1.py
@@ -11,7 +11,7 @@ import statistics as st
 def main():
     
     dataset_1 = pd.read_csv('NYC_Bicycle_Counts_2016_Corrected.csv')
-    dataset_1['Brooklyn Bridge'] = pd.to_numeric(dataset_1['Brooklyn Bridge'].replace(',','', regex=True))
+    dataset_1['Brooklyn Bridge']      = pd.to_numeric(dataset_1['Brooklyn Bridge'].replace(',','', regex=True))
     dataset_1['Manhattan Bridge']     = pd.to_numeric(dataset_1['Manhattan Bridge'].replace(',','', regex=True))
     dataset_1['Queensboro Bridge']    = pd.to_numeric(dataset_1['Queensboro Bridge'].replace(',','', regex=True))
     dataset_1['Williamsburg Bridge']  = pd.to_numeric(dataset_1['Williamsburg Bridge'].replace(',','', regex=True))
@@ -26,7 +26,7 @@ def main():
     
     print("")
     print("Problem 2:")
-    
+    # print(user_input())
     temp(dataset_1)
 
     print("")
@@ -38,88 +38,107 @@ def main():
     return 0
 
 # First Problem
+def data_preprocessing(array):
+    arr = np.array(array)
+    q3, q1 = np.percentile(arr, (75, 25))
+    iqr = q3 - q1
+    new_q3 = q3 + 1.5 * iqr
+    new_q1 = q1 - 1.5 * iqr
+    new_arr = arr
+    new_arr = arr[(arr > new_q1) & (arr < new_q3)]
+
+    return new_arr
 
 def R_2_best_bridges(dataset_1):
 
-    #Calculating top three bridges using R-squared method
-    r2_list = [0]*4
-    a = [0]*len(dataset_1)
+    # Calculating top three bridges using R-squared method after preprocessing the data using IQR
+
+    new_arr1 = data_preprocessing(dataset_1['Brooklyn Bridge'])
+    a1 = [0]*len(new_arr1)
     count = 1
-    pred = [0]*len(dataset_1)
-    for i in range(len(a)):
-        a[i] += count
-        count += 1
+    for i in range(len(a1)):
+            a1[i] += count
+            count += 1
 
+    r2_list = [0]*4
 
-    # R_2 plot for Brooklyn Bridge
-    x = [x for x in range(len(dataset_1['Brooklyn Bridge']))]
-    z = np.polyfit([x for x in range(len(dataset_1['Brooklyn Bridge']))], dataset_1["Brooklyn Bridge"],1)
+    x1 = [x for x in range(len(new_arr1))]
+    pred1 = [0]*len(new_arr1)
+    z = np.polyfit([x for x in range(len(new_arr1))], new_arr1,1)
     p = np.poly1d(z)
-    
-    #list of trendline(prediction) data
-    for i in range(len(dataset_1)):
-        pred[i] = z[0] * a[i] + z[1]
-    trendline = r2_score(dataset_1["Brooklyn Bridge"], pred)
-    r2_list[0] = trendline
-    # print(trendline)
 
     fg1 = mp.subplot(2, 2, 1)
-    fg1.text(0.17, 0.93, 'R-squared = %0.4f' % trendline, horizontalalignment='center', verticalalignment='center', transform=fg1.transAxes, fontsize = 12, bbox=dict(facecolor='red', alpha=0.5))
-    mp.plot([x for x in range(len(dataset_1['Brooklyn Bridge']))], dataset_1["Brooklyn Bridge"], 'c')
-    mp.plot(x, p(x), 'r')
-    
+    mp.plot([x for x in range(len(new_arr1))], new_arr1 , color = 'c')
+    mp.plot(x1, p(x1), 'r')
 
-    # R_2 plot for Manhattan Bridge
-    x = [x for x in range(len(dataset_1['Manhattan Bridge']))]
-    z = np.polyfit([x for x in range(len(dataset_1['Manhattan Bridge']))], dataset_1["Manhattan Bridge"],1)
+    for i in range(len(new_arr1)):
+        pred1[i] = z[0] * a1[i] + z[1] 
+    r2_list[0] = r2_score(new_arr1, pred1)
+    fg1.text(0.17, 0.93, 'R-squared = %0.4f' % r2_list[0], horizontalalignment='center', verticalalignment='center', transform=fg1.transAxes, fontsize = 12, bbox=dict(facecolor='red', alpha=0.5))
+
+
+    new_arr2 = data_preprocessing(dataset_1['Manhattan Bridge'])
+    a2 = [0]*len(new_arr2)
+    count = 1
+    for i in range(len(a2)):
+            a2[i] += count
+            count += 1
+
+    x2 = [x for x in range(len(new_arr2))]
+    pred2 = [0]*len(new_arr2)
+    z = np.polyfit([x for x in range(len(new_arr2))], new_arr2,1)
     p = np.poly1d(z)
-
-    for i in range(len(dataset_1)):
-        pred[i] = z[0] * a[i] + z[1]
-    trendline = r2_score(dataset_1["Manhattan Bridge"], pred)
-    r2_list[1] = trendline
-    # print(trendline)
-
     fg2 = mp.subplot(2, 2, 2)
-    fg2.text(0.17, 0.93, 'R-squared = %0.4f' % trendline, horizontalalignment='center', verticalalignment='center', transform=fg2.transAxes, fontsize = 12, bbox=dict(facecolor='red', alpha=0.5))
-    mp.plot([x for x in range(len(dataset_1['Manhattan Bridge']))], dataset_1["Manhattan Bridge"], 'c')
-    mp.plot(x, p(x), 'r')
+    mp.plot([x for x in range(len(new_arr2))], new_arr2, color = 'c')
+    mp.plot(x2, p(x2), 'r')
+
+    for i in range(len(new_arr2)):
+        pred2[i] = z[0] * a2[i] + z[1] 
+    r2_list[1] = r2_score(new_arr2, pred2)
+    fg2.text(0.17, 0.93, 'R-squared = %0.4f' % r2_list[1], horizontalalignment='center', verticalalignment='center', transform=fg2.transAxes, fontsize = 12, bbox=dict(facecolor='red', alpha=0.5))
 
 
-    # R_2 plot for Williamsburg Bridge
-    x = [x for x in range(len(dataset_1['Williamsburg Bridge']))]
+    new_arr3 = data_preprocessing(dataset_1['Queensboro Bridge'])
+    a3 = [0]*len(new_arr3)
+    count = 1
+    for i in range(len(a3)):
+            a3[i] += count
+            count += 1
 
-    z = np.polyfit([x for x in range(len(dataset_1['Williamsburg Bridge']))], dataset_1["Williamsburg Bridge"],1)
+    x3 = [x for x in range(len(new_arr3))]
+    pred3 = [0]*len(new_arr3)
+    z = np.polyfit([x for x in range(len(new_arr3))], new_arr3,1)
     p = np.poly1d(z)
-
-    for i in range(len(dataset_1)):
-        pred[i] = z[0] * a[i] + z[1]
-    trendline = r2_score(dataset_1["Williamsburg Bridge"], pred)
-    r2_list[2] = trendline
-    # print(trendline)
-
     fg3 = mp.subplot(2, 2, 3)
-    fg3.text(0.17, 0.93, 'R-squared = %0.4f' % trendline, horizontalalignment='center', verticalalignment='center', transform=fg3.transAxes, fontsize = 12, bbox=dict(facecolor='red', alpha=0.5))
-    mp.plot([x for x in range(len(dataset_1['Williamsburg Bridge']))], dataset_1["Williamsburg Bridge"], 'c')
-    mp.plot(x, p(x), 'r')
+    mp.plot([x for x in range(len(new_arr3))], new_arr3, color = 'c')
+    mp.plot(x3, p(x3), 'r')
+
+    for i in range(len(new_arr3)):
+        pred3[i] = z[0] * a3[i] + z[1] 
+    r2_list[2] = r2_score(new_arr3, pred3)
+    fg3.text(0.17, 0.93, 'R-squared = %0.4f' % r2_list[2], horizontalalignment='center', verticalalignment='center', transform=fg3.transAxes, fontsize = 12, bbox=dict(facecolor='red', alpha=0.5))
 
 
-    # R_2 plot for Queensboro Bridge
-    x = [x for x in range(len(dataset_1['Queensboro Bridge']))]
+    new_arr4 = data_preprocessing(dataset_1['Williamsburg Bridge'])
+    a4 = [0]*len(new_arr4)
+    count = 1
+    for i in range(len(a4)):
+            a4[i] += count
+            count += 1
 
-    z = np.polyfit([x for x in range(len(dataset_1['Queensboro Bridge']))], dataset_1["Queensboro Bridge"],1)
+    x4 = [x for x in range(len(new_arr4))]
+    pred4 = [0]*len(new_arr4)
+
+    z = np.polyfit([x for x in range(len(new_arr4))], new_arr4,1)
     p = np.poly1d(z)
-
-    for i in range(len(dataset_1)):
-        pred[i] = z[0] * a[i] + z[1]
-    trendline = r2_score(dataset_1["Queensboro Bridge"], pred)
-    r2_list[3] = trendline
-    # print(trendline)
-
     fg4 = mp.subplot(2, 2, 4)
-    fg4.text(0.17, 0.93, 'R-squared = %0.4f' % trendline, horizontalalignment='center', verticalalignment='center', transform=fg4.transAxes, fontsize = 12, bbox=dict(facecolor='red', alpha=0.5))
-    mp.plot([x for x in range(len(dataset_1['Queensboro Bridge']))], dataset_1["Queensboro Bridge"], 'c')
-    mp.plot(x, p(x), 'r')
+    mp.plot([x for x in range(len(new_arr4))], new_arr4, color = 'c')
+    mp.plot(x4, p(x4), 'r')
+
+    for i in range(len(new_arr4)):
+        pred4[i] = z[0] * a4[i] + z[1] 
+    r2_list[3] = r2_score(new_arr4, pred4)
+    fg4.text(0.17, 0.93, 'R-squared = %0.4f' % r2_list[3], horizontalalignment='center', verticalalignment='center', transform=fg4.transAxes, fontsize = 12, bbox=dict(facecolor='red', alpha=0.5))
 
     mp.suptitle('R-squared Plot for each Bridge', fontsize = 15)
     fg1.title.set_text("Brooklyn")
@@ -128,14 +147,14 @@ def R_2_best_bridges(dataset_1):
     fg2.title.set_text("Manhattan")
     fg2.set_ylabel('Traffic in each Day')
     fg2.set_xlabel("Day Since April 1st")
-    fg3.title.set_text("Williamsburg")
+    fg3.title.set_text("Quuensboro")
     fg3.set_ylabel('Traffic in each Day')
     fg3.set_xlabel("Day Since April 1st")
-    fg4.title.set_text("Brooklyn")
+    fg4.title.set_text("Williamsburg")
     fg4.set_ylabel('Traffic in each Day')
     fg4.set_xlabel("Day Since April 1st")
     mp.subplots_adjust(left = 0.1, bottom = 0.1, right = 0.9, top = 0.9, wspace = 0.3, hspace = 0.3)
-    # mp.show()
+    mp.show()
     
     least_r2_index = r2_list.index(min(r2_list))
     return(least_r2_index)
@@ -157,7 +176,7 @@ def Total_best_bridges(dataset_1):
     mp.xlabel('Name of Bridges')
     mp.ylabel('Total Number of Bicyclists (million)')
     mp.title('Total Number of Bicyclists for each Bridge', fontsize = 15)
-    # mp.show()
+    mp.show()
 
     least_total_index = Bridge_list.index(min(Bridge_list))
 
@@ -182,11 +201,15 @@ def sort(list1, list2):
 
 def temp(dataset_1):
 
+    # # high temperature IQR
+
+    # # low temperature IQR
+
     # finds the average temperature in each day
     avg_temp = [0]*len(dataset_1)
 
     for i in range(len(avg_temp)):
-        avg_temp[i] = (dataset_1['High Temp'][i] + dataset_1['Low Temp'][i]) / 2
+        avg_temp[i] = (dataset_1['High Temp'][i] + dataset_1['Low Temp'][i]) / 2 #여기 고쳐아함!!!!!
 
 
     # gets new total list excluding Brooklyn bridge
@@ -212,16 +235,6 @@ def temp(dataset_1):
     #sorting data
     x_p, y_precipitation = sort(x_p, y_precipitation)
     x_np, y_noprecipitation = sort(x_np, y_noprecipitation)
-
-    # low_temp_total = new_total
-    # high_temp_total = new_total
-
-    low_temp_sorted, low_temp_total = sort(list(dataset_1['Low Temp']), new_total)
-    high_temp_sorted, high_temp_total = sort(list(dataset_1['High Temp']), new_total)
-    avg_temp_sorted, new_total_sorted = sort(avg_temp, new_total)
-
-    # print(avg_temp, new_total)
-    # print(avg_temp_sorted, new_total_sorted)
     
     # # trendlines
     z_p = np.polyfit(x_p, y_precipitation,2)
@@ -229,48 +242,63 @@ def temp(dataset_1):
     z_np = np.polyfit(x_np, y_noprecipitation,2)
     p_np = np.poly1d(z_np)
 
-    z_ltemp = np.polyfit(dataset_1['Low Temp'], new_total, 2)
-    p_ltemp = np.poly1d(z_ltemp)
-    z_htemp = np.polyfit(dataset_1['High Temp'], new_total, 2)
-    p_htemp = np.poly1d(z_htemp)
-
-    z_avg = np.polyfit(avg_temp, new_total, 2)
-    p_avg = np.poly1d(z_avg)
-
     mp.figure()
     mp.suptitle('Impact of Weather Condition on Bike Traffic', fontsize = 15)
+    mp.subplots_adjust(left = 0.1, bottom = 0.1, right = 0.9, top = 0.9, wspace = 0.3, hspace = 0.3)
 
-    fg1 = mp.subplot(2,2,1)
-    mp.plot(dataset_1['Low Temp'], p_ltemp(dataset_1['Low Temp']), 'r')
-    mp.scatter(dataset_1['Low Temp'], new_total, color = 'c')
-    fg1.set_ylabel('Total Traffic')
-    fg1.set_xlabel('Daily Low Temperature (°F)')
-
-
-    fg2 = mp.subplot(2,2,2)
-    mp.plot(dataset_1['High Temp'], p_htemp(dataset_1['High Temp']), 'r')
-    mp.scatter(dataset_1['High Temp'], new_total, color = 'c')
-    fg2.set_ylabel('Total Traffic')
-    fg2.set_xlabel('Daily High Temperature (°F)')
-
-    fg3 = mp.subplot(2,2,3)
-    mp.plot(avg_temp, p_avg(avg_temp), 'r')
-    mp.scatter(avg_temp, new_total, color = 'c')
-    fg3.set_ylabel('Total Traffic')
-    fg3.set_xlabel('Daily Average Temperature (°F)')
-
-    mp.subplot(2,2,4)
-    mp.plot(x_p, p_p(x_p), 'r')
-    mp.plot(x_np, p_np(x_np), 'b')
-    mp.xlabel('Daily Average Temperature (°F)')
-    mp.ylabel('Total Traffic')
-    mp.title('Impact of Daily Average Temperature and Percipitation on Total Traffic')
+    mp.subplot(3,1,1)
+    x_1 = np.array(x_p).reshape((-1,1))
+    y_1 = np.array(y_precipitation)
+    model = LinearRegression()
+    model.fit(x_1, y_1)
+    # r_sq = model.score(x_1, y_1)
+    # print(r_sq)
+    # print("coefficient:", model.coef_)
+    # print("y-intercent:", model.intercept_)
     mp.scatter(x_p, y_precipitation, label='Precipitation')
-    mp.scatter(x_np, y_noprecipitation, label='No Precipitation')
+    mp.plot(x_1, model.predict(x_1), label = 'Linear Regression', color = 'green')
+    mp.plot(x_p, p_p(x_p),label = 'Trendline', color = 'r')
+    mp.xlabel('Daily Average Temperature (°F)')
+    mp.ylabel('Total Bike Traffic (24 hr period)')
     mp.legend()
 
-    mp.subplots_adjust(left = 0.1, bottom = 0.1, right = 0.9, top = 0.9, wspace = 0.3, hspace = 0.3)
+    mp.subplot(3,1,2)
+    x_2 = np.array(x_np).reshape((-1,1))
+    y_2 = np.array(y_noprecipitation)
+    model = LinearRegression()
+    model.fit(x_2, y_2)
+    # print("coefficient:", model.coef_)
+    # print("y-intercent:", model.intercept_)
+    mp.scatter(x_np, y_noprecipitation, label='No Precipitation', color = 'darkorange')
+    mp.plot(x_2, model.predict(x_2), label = 'Linear Regression', color = 'green')
+    mp.plot(x_np, p_np(x_np),label = 'Trendline', color = 'r')
+    mp.xlabel('Daily Average Temperature (°F)')
+    mp.ylabel('Total Bike Traffic (24 hr period)')
+    mp.legend()
+
+    mp.subplot(3,1,3)
+    mp.scatter(x_p, y_precipitation, label='Precipitation')
+    mp.scatter(x_np, y_noprecipitation, label='No Precipitation', color = 'darkorange')
+    mp.xlabel('Daily Average Temperature (°F)')
+    mp.ylabel('Total Bike Traffic (24 hr period)')
+    mp.legend()
     mp.show()
+    
+    return 0
+
+def user_input():
+    pre = float(input("Enter Precipitation: "))
+    high_temp = float(input('Enter Highest Temperature: '))
+    low_temp = float(input('Enter Lowest Temperature: '))
+
+    avg_temp = (high_temp + low_temp) / 2
+
+    if pre > 0:
+        total = (257.42705815 * avg_temp  - 5250.524153541815)
+    else:
+        total = (136.48628816 * avg_temp + 7713.267222284023)
+
+    return total
 
 # Third Problem
 
--- sandbox.py
+++ sandbox.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import math as m
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as mp
 
 data = pd.read_csv('NYC_Bicycle_Counts_2016_Corrected.csv')
 dataset_1 = pd.read_csv('NYC_Bicycle_Counts_2016_Corrected.csv')
@@ -12,22 +13,5 @@ dataset_1['Queensboro Bridge']    = pd.to_numeric(dataset_1['Queensboro Bridge']
 dataset_1['Williamsburg Bridge']  = pd.to_numeric(dataset_1['Williamsburg Bridge'].replace(',','', regex=True))
 dataset_1['Total']                = pd.to_numeric(dataset_1['Total'].replace(',','', regex=True))
 
-# print(data.to_string())
-
-# R_square = r2_score(a, dataaa) 
-# print('Coefficient of Determination', R_square)
-
-#initiate linear regression model
-model = LinearRegression()
-
-#define predictor and response variables
-X, y = [dataset_1['Brooklyn Bridge']], dataset_1.score
-
-#fit regression model
-model.fit(X, y)
-
-#calculate R-squared of regression model
-r_squared = model.score(X, y)
-
-#view R-squared value
-print(r_squared)
+name = input("what is your name? ")
+print(name)
